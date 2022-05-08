28ec4293c3481461adfadfa928623c36ddf250e1
ziadshimy7 <ziadshimy7@gmail.com>
2022-05-08 07:21:16 +0500

    refactored navbar menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import logo from "../../assets/main-logo.png";
 import { FiSearch } from "react-icons/fi";
 import { AiOutlineMenu } from "react-icons/ai";
-import profileImage from "../../assets/test-profile.svg";
+import NavbarMenu from "./NavbarMenu";
 const Navbar = () => {
   const [toggleNavMenu, setToggleNavMenu] = useState(false);
   const onNavClickHandler = () => {
@@ -29,21 +29,7 @@ const Navbar = () => {
           />
         </div>
       </nav>
-      {toggleNavMenu && (
-        <div className="absolute h-[370px] bg-gray-900 w-full flex flex-col items-center">
-          <button className="flex justify-center items-center gap-2 bg-gray-800 py-2 rounded-lg w-[95%]">
-            <img src={profileImage} alt="profile" />
-            <span className="text-white font-graphik text-sm ">
-              Sign Up or Log in
-            </span>
-          </button>
-          <div className="w-[95%] flex flex-col border-gray-500-2 gap-2 divide-y">
-            <p className="text-white py-2">Plants and flowers</p>
-            <p className="text-white py-2">Plant care</p>
-            <p className="text-white py-2">News</p>
-          </div>
-        </div>
-      )}
+      {toggleNavMenu && <NavbarMenu />}
     </div>
   );
 };
---
+++ src/components/Navbar/NavbarMenu.js
@@ -0,0 +1,22 @@
+import React from "react";
+import profileImage from "../../assets/test-profile.svg";
+
+const NavbarMenu = () => {
+  return (
+    <div className="absolute h-[370px] bg-gray-900 w-full flex flex-col items-center">
+      <button className="flex justify-center items-center gap-2 bg-gray-800 py-2 rounded-lg w-[95%]">
+        <img src={profileImage} alt="profile" />
+        <span className="text-white font-graphik text-sm ">
+          Sign Up or Log in
+        </span>
+      </button>
+      <div className="w-[95%] flex flex-col border-gray-500-2 gap-2 divide-y">
+        <p className="text-white py-2">Plants and flowers</p>
+        <p className="text-white py-2">Plant care</p>
+        <p className="text-white py-2">News</p>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarMenu;
