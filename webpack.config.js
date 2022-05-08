b1b0c493d20e84a798feb9e039257eb1e6f7e3b3
ziadshimy7 <ziadshimy7@gmail.com>
2022-05-09 00:01:36 +0500

    webpack sass config

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
