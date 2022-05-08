1f517ca2b20e6b30b7b2d12b8496aec43528b0e7
ziadshimy7 <ziadshimy7@gmail.com>
2022-05-09 00:06:37 +0500

    webpack font configs

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
       {
